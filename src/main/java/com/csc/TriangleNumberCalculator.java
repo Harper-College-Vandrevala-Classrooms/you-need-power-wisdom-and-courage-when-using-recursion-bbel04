be9b114282409c5f687c661f0c5ad742b25ada45
bbel04 <158131166+bbel04@users.noreply.github.com>
2024-10-20 16:03:17 -0500

    Implement value, add, subtract methods
    Implement methods in TriangleNumberCalculator.java, add JUnit test cases

--- src/main/java/com/csc/TriangleNumberCalculator.java
+++ src/main/java/com/csc/TriangleNumberCalculator.java
@@ -1,5 +1,34 @@
 package com.csc;
 
 public class TriangleNumberCalculator {
-  // Your code goes here!
+  public static void main(String[] args) {
+    int triangleNumber = 3;
+
+    System.out.println(value(triangleNumber));  // This should print out 
+
+    System.out.println(add(1,1));  // This should print out 2
+    System.out.println(add(2, 3)); // This should print out 9
+    System.out.println(add(4, 2)); // This should print out 13
+
+    System.out.println(subtract(1, 1)); // This should print out 0
+    System.out.println(subtract(2, 3)); // This should print out -3
+    System.out.println(subtract(4, 2)); // This should print out 7
+  }
+
+  static int value(int n) {
+    if (n <= 1) {    // Base case
+      return 1;
+    }
+
+    return(value(n - 1) + n);    // recursive call; add each integer up to and including the nth
+  }
+
+  static int add(int n, int m) {
+    return (value(n) + value(m));
+  }
+
+  static int subtract(int n, int m) {
+    return (value(n) - value(m));
+  }
 }
+
